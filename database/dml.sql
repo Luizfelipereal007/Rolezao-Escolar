30e0998d9b58ad0fe004f4f72f32e772fe5b4e6a
Luan Moura <luan54098746@aluno.ms.senac.br>
2025-12-02 20:58:47 -0400

    adiiconando dml

---
+++ database/dml.sql
@@ -0,0 +1,8 @@
+INSERT INTO instituicao (nome, localizacao, cnpj)
+VALUES ('IFMS Campo Grande', 'Campo Grande - MS', '00.000.000/0001-00');
+
+INSERT INTO professor (id_instituicao, nome, senha)
+VALUES (1, 'João da Silva', '123');
+
+INSERT INTO ponto_turistico (nome, local, descricao, custo)
+VALUES ('Trilha da Serra', 'Bonito - MS', 'Trilha leve com cachoeira', 20.00);

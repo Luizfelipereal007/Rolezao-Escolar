9a94f32c63d8b5bf1182f16eebe1d5a416e93ae2
Luizfelipereal007 <lmarianobonfim@gmail.com>
2025-12-02 21:34:01 -0400

    feat(model): :sparkles: add CRUD tourist points

--- app/model/pontoTuristicoModel.php
+++ app/model/pontoTuristicoModel.php
@@ -27,4 +27,54 @@ class PontoTuristicoModel
             return $erro;
         }
     }
+    public function listarPontosTuristicos()
+    {
+        try {
+            $query = "SELECT * FROM $this->tabela ORDER BY nome";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (Exception $e) {
+            $erro = $e->getMessage();
+            return $erro;
+        }
+    }
+
+    public function atualizarPontoTuristico($id_ponto_turistico, $local, $nome, $descricao, $custo)
+    {
+        try {
+            $query = "UPDATE $this->tabela 
+                SET local = :local, nome = :nome, descricao = :descricao, custo = :custo 
+                WHERE id_ponto_turistico = :id_ponto_turistico
+            ";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id_ponto_turistico', $id_ponto_turistico);
+            $stmt->bindParam(':local', $local);
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':descricao', $descricao);
+            $stmt->bindParam(':custo', $custo);
+            $stmt->execute();
+            return true;
+
+        } catch (Exception $e) {
+            $erro = $e->getMessage();
+            return $erro;
+        }
+    }
+
+    public function excluirPontoTuristico($id_ponto_turistico)
+    {
+        try {
+            $query = "DELETE FROM $this->tabela WHERE id_ponto_turistico = :id_ponto_turistico";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id_ponto_turistico', $id_ponto_turistico);
+            $stmt->execute();
+            return true;
+
+        } catch (Exception $e) {
+            $erro = $e->getMessage();
+            return $erro;
+        }
+    }
 }
